4a8ef608f1021ea594b5cf3f014795546f0be23e
Luca Fungo <alfieriluca91@gmail.com>
2023-02-16 22:08:35 +0000

    colorama-try

---
+++ main.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+# print(Fore.BLACK+Back.GREEN + 'Hello word!')
+# print(Fore.BLACK+Back.YELLOW + 'Hello word!')
+# print(Fore.BLACK+Back.WHITE + 'Hello word!')
