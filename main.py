7c099f86af15aef4221fdf76772b8fce4a79f1e7
Luca Fungo <alfieriluca91@gmail.com>
2023-02-16 23:53:01 +0000

    work as intended score included

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ colorama.init(autoreset=True)
 # print(Fore.BLACK+Back.YELLOW + 'Hello word!')
 # print(Fore.BLACK+Back.WHITE + 'Hello word!')
 
-score = 0
 attempts = 0
 
 word_of_the_day = 'abcde'
@@ -17,10 +16,13 @@ def slicing(word):
 
 
 def check_attempt(word_of_the_day, word_by_user):
+    global score
+    score = 0
     count = 0
     for user_letter in word_by_user:
         if user_letter == word_of_the_day[count]:
             print(Fore.BLACK+Back.GREEN + user_letter)
+            score = score + 1
             count = count + 1
         elif user_letter in word_of_the_day:
             print(Fore.BLACK+Back.YELLOW + user_letter)
@@ -28,16 +30,19 @@ def check_attempt(word_of_the_day, word_by_user):
         else:
             print(Fore.BLACK+Back.WHITE + user_letter)
             count = count + 1
+    return score
 
 
 slicing(word_of_the_day)
 
 while attempts < 5:
     word_by_user = input('Type your guess ')
-    slicing(word_by_user)
-    check_attempt(word_of_the_day, word_by_user)
-    attempts = attempts + 1
-    print(score)
-    # print(slicing(word_by_user))
-    # print(slicing(word_of_the_day))
-    # print(attempts)
+    if len(word_by_user) == 5:
+        slicing(word_by_user)
+        check_attempt(word_of_the_day, word_by_user)
+        attempts = attempts + 1
+        if score == 5:
+            print('YOU WON!')
+            break
+    else:
+        print('Please, type a five letters word')
