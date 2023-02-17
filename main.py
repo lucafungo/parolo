f1cd4a159bbaf56b9dea034115aa91e7542d5a47
Luca Fungo <alfieriluca91@gmail.com>
2023-02-17 00:18:18 +0000

    rubbish removed

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
-# print(Fore.BLACK+Back.GREEN + 'Hello word!')
-# print(Fore.BLACK+Back.YELLOW + 'Hello word!')
-# print(Fore.BLACK+Back.WHITE + 'Hello word!')
 
 attempts = 0
 
